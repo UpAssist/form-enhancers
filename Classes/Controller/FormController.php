c79deafa5f282e935d2490eafc27cd579cd37b6d
Henjo Hoeksma <henjo@upassist.com>
2017-06-15 17:58:17 +0200

    General features as provided by the Formbuilder bundle package except without hooking it up to formbuilder

---
+++ Classes/Controller/FormController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace UpAssist\FormEnhancers\Controller;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\Controller\ActionController;
+
+class FormController extends ActionController
+{
+
+    /**
+     * @Flow\Inject
+     * @var \TYPO3\Form\Persistence\FormPersistenceManagerInterface
+     */
+    protected $formPersistenceManager;
+
+    /**
+     * @deprecated Deprecated as of Neos 1.2.0
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->response->setHeader('Content-Type', 'application/json');
+
+        return json_encode($this->formPersistenceManager->listForms());
+    }
+
+}
